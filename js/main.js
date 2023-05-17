fe8b793e2cedaa62ec0ec6d48299826334138889
tlcghost <tlcghost@proton.me>
2023-05-17 19:09:36 +0000

    Audio should play on-click, enjoy : )

--- js/main.js
+++ js/main.js
@@ -12,19 +12,23 @@ document.getElementById("fullscreen-image").addEventListener("click", function()
   document.getElementById("fullscreen-image").style.display = "none";
 });
 
+var playing = false;
 
-var audioPlayer = document.getElementById("audio-player");
-var playButton = document.getElementById("play-button");
 
-playButton.addEventListener("click", function() {
-  audioPlayer.play();
-  playButton.style.display = "none";
+window.addEventListener('click', () => {
+  var audioPlayer = document.getElementById("audio-player");
+  audioPlayer.muted = false;
+
+  if(playing == false) {
+    playedPromise = audioPlayer.play();
+    playing = true;
+    
+    if (playedPromise) {
+        playedPromise.catch((e) => {
+          playing = false
+          console.log("Can't click :L");
+         });
+     }
+  }
 });
 
-audioPlayer.addEventListener("play", function() {
-  playButton.style.display = "none";
-});
-
-audioPlayer.addEventListener("pause", function() {
-  playButton.style.display = "block";
-});
